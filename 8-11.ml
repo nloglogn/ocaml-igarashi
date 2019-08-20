34e6c8b9629f87c5067025c8b33d9bfa2db811d5
nloglogn <46310442+nloglogn@users.noreply.github.com>
2019-08-20 17:56:11 +0900

    display line number

--- 8-11.ml
+++ 8-11.ml
@@ -1,8 +1,12 @@
 let display_file filename =
   let file = open_in filename in
+  let i = ref 0 in 
   let flag = ref true in
   while !flag do
     begin
+      i := !i + 1;
+      print_int !i;
+      print_string " ";
       try print_endline (input_line file);
       with End_of_file -> flag := false;
     end
